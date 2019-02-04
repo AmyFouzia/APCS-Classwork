f1a68eceb4c65b2ecef201ba3699cfb2e8299ee8
Amy Fouzia <afouzia00@stuy.edu>
2019-02-04 14:52:28 -0500

    classwork

--- classwork.java
+++ classwork.java
@@ -1,1 +1,25 @@
-j
+import java.util.*;
+
+public class classwork{
+
+  public static List<String> makeAllWords(int k){
+    List<String> words = new ArrayList<String>();
+    mAWHelp(k, "a", words);
+    return words;
+  }
+
+  public static void mAWHelp(int k, String partial, List<String> words){
+    if (k == 0){
+      words.add(partial);
+    }
+    else{
+      mAWHelp(k-1, (partial + 1), words);
+    }
+  }
+
+  public static void main(String[] args){
+      System.out.println(makeAllWords(3));
+  }
+
+
+}
